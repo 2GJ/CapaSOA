6363dcb50f67143a5dbda39fd984f77d5f0a67aa
2GJ <GermanGJ@Hotamil.com>
2018-01-26 17:43:43 -0500

    20180126 - 2GJ Cierre del dia.

--- CapaSOA2GJ/CapaFacade2GJ.cs
+++ CapaSOA2GJ/CapaFacade2GJ.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using Tools2GJ;
 
 namespace CapaSOA2GJ
 {
-    class CapaFacade2GJ
+    public class CapaFacade2GJ
     {
+        public DataSet QryFastByDS(String In_Qry)
+        {
+            String2GJ Obj2GJ = new String2GJ();
+            refFacade2GJBA.Facade2GJBA objFac = new refFacade2GJBA.Facade2GJBA();
+            return objFac.QryFastStrByDS(Obj2GJ.ConvStringToBase64UTF8(In_Qry));
+        }
     }
 }
